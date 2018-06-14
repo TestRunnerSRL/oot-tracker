677a19adda5a9f0b8b8dd5abd10b14b0f650622d
Aharon Turpie <aaturpie@verizon.net>
2018-06-14 02:33:54 -0400

    Fix Firefox zoom support

--- script/main.js
+++ script/main.js
@@ -174,8 +174,10 @@ function setOrientation() {
 
 function setZoom(target, sender) {
     document.getElementById(target).style.zoom = sender.value / 100;
+    document.getElementById(target).style.zoom = sender.value / 100;
 
-    //    -moz-transform: scale(0.5);
+    document.getElementById(target).style.MozTransform = "scale(" + (sender.value / 100) + ")";
+    document.getElementById(target).style.MozTransformOrigin = "0 0";
 
     document.getElementById(target + 'size').innerHTML = (sender.value) + '%';
     saveCookie();
