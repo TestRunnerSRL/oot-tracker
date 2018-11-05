14d1d8dd770ca90a87b26cb8e26fe075735f958e
Stephen Rollins <stephen@lucidchart.com>
2018-11-05 07:51:11 -0700

    Add location for Saria on the bridge
    Relevant if Ocarinas are shuffled

--- script/chests.js
+++ script/chests.js
@@ -1108,5 +1108,13 @@ var chests = [
             return "unavailable";
         },
     },
+    {
+        name: "Saria on the Bridge",
+        x: "74.5%",
+        y: "57.5%",
+        isAvailable: function() {
+            return "available";
+        },
+    },
 ]
 
